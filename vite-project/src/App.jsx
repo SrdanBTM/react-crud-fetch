0f6496a40ff726736886392a57d66ab8c4cac6a3
Srdan Cakalj <srdjancakalj14@gmail.com>
2026-02-09 19:22:12 +0100

    feat: fetch products and handle loading/error states in App

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,53 @@
 
+import { useState, useEffect } from 'react'
 
 
 function App() {
 
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
+
+
+  useEffect(() => {
+    async function load() {
+      try {
+        const response = await fetch('https://dummyjson.com/products')
+        if (!response.ok) {
+          throw new Error('Failed to fetch items')
+        }
+        const data = await response.json()
+        setItems(data.products)
+      } catch (err) {
+        setIsError(true)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    load()
+  }, [])
+
+
+
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+
+  if (isError) {
+    return <p>Error</p>
+  }
+
+
   return (
     <div>
-      test
+      
     </div>
   )
 }
 
 export default App
+
+
+
+
