0b700c1f56f0359466b2d35b6ab3a4e9b7db8f74
Srdan Cakalj <srdjancakalj14@gmail.com>
2026-02-12 18:58:49 +0100

    feat: update Button component

--- vite-project/src/components/ui/button/Button.jsx
+++ vite-project/src/components/ui/button/Button.jsx
@@ -3,9 +3,14 @@
 import styles from './button.module.css'
 
 
-export default function Button({title, variant}) {
+export default function Button({ title, variant, onClick }) {
 
   return (
-    <button className={`${styles.button} ${styles[variant]}`}>{title}</button>
+    <button
+      className={`${styles.button} ${styles[variant]}`}
+      onClick={onClick}
+    >
+      {title}
+    </button>
   )
 }
