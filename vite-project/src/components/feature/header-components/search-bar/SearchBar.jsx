b7f0aea2df06c71240fd9a8ca6310b9323994882
Srdan Cakalj <srdjancakalj14@gmail.com>
2026-02-13 12:38:07 +0100

    refactor: remove unused container element

--- vite-project/src/components/feature/header-components/search-bar/SearchBar.jsx
+++ vite-project/src/components/feature/header-components/search-bar/SearchBar.jsx
@@ -6,12 +6,10 @@ import styles from './searchBar.module.css'
 export default function SearchBar() {
 
   return (
-    <div className={styles.container}>
       <input 
         className={styles.input}
         type='text'
         placeholder='Search by title...'
       />
-    </div>
   )
 }
