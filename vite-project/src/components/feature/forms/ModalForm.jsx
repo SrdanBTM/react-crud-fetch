446f6c3113622638ba5567703efd2945cf216f4b
Srdan Cakalj <srdjancakalj14@gmail.com>
2026-02-13 11:46:45 +0100

    feat: add ModalForm component

---
+++ vite-project/src/components/feature/forms/ModalForm.jsx
@@ -0,0 +1,18 @@
+
+
+import styles from './modalForm.module.css'
+import Input from '../../ui/input/Input.jsx'
+import Select from '../../ui/select/Select.jsx'
+
+
+export default function ModalForm({ categoryOptions }) {
+
+  return (
+    <form className={styles.form}>
+      <Select options={categoryOptions} />
+      <Input />
+      <Input />
+      <Input />
+    </form>
+  )
+}
